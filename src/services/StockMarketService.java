35a375f5b9b45f8da28dd907ad677eb4fa43f7da
aimo0001 <aimo0001@stud.kea.dk>
2025-05-07 17:36:40 +0200

    videre på service.

--- src/services/StockMarketService.java
+++ src/services/StockMarketService.java
@@ -1,4 +1,21 @@
 package services;
 
+import models.StockMarket;
+import repositories.StockMarketRepository;
+
+import java.util.List;
+
 public class StockMarketService {
+    private StockMarketRepository stockMarketRepository;
+    private List<StockMarket> stockMarkets;
+
+    public StockMarketService(StockMarketRepository stockMarketRepository){
+        this.stockMarketRepository = stockMarketRepository;
+        stockMarkets = stockMarketRepository.getStockMarket();
+    }
+
+    public StockMarketRepository getStockMarketRepository() {
+        return stockMarketRepository;
+    }
+
 }
--- src/services/UserService.java
+++ src/services/UserService.java
@@ -1,4 +1,18 @@
 package services;
 
+import models.User;
+import repositories.UserRepository;
+
+import java.util.List;
+
 public class UserService {
+    private List<User> users;
+    private UserRepository userRepo;
+
+    public UserService(UserRepository) {
+        this.userRepo = userRepo;
+        userRepo.readFile();
+        users = userRepo.getUsers();
+    }
+
 }
