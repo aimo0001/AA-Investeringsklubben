2067c618909e6ef5f6f1429d83981691f8ac10e6
aimo0001 <aimo0001@stud.kea.dk>
2025-05-07 17:29:10 +0200

    model classes + lidt service

--- src/models/Transaction.java
+++ src/models/Transaction.java
@@ -3,8 +3,40 @@ package models;
 public class Transaction {
     private int id;
     private User userID;
-    private String stockID;
+    private String ticker;
     private double price;
     private boolean orderType;
     private int quantity;
+
+    public Transaction (String ticker, double price, boolean orderType, int quantity) {
+        this.ticker = ticker;
+        this.price = price;
+        this.orderType = orderType;
+
+    }
+
+    public String getTicker() {
+        return ticker;
+
+    }
+
+    public void setTicker(String ticker) {
+        this.ticker = ticker;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
--- src/models/User.java
+++ src/models/User.java
@@ -15,53 +15,61 @@ public class User {
         this.userID = userID;
         this.fullName = fullName;
         this.email = email;
-        this.birthDate=birthDate;
+        this.birthDate=LocalDate.parse(birthDate);
         this.balance = balance;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
+        this.createdAt = LocalDate.parse(createdAt.toString());
+        this.updatedAt = LocalDate.parse(updatedAt.toString());
     }
+
+
     public int getUserID() {
         return userID;
-
     }
     public void setUserID(int userID) {
         this.userID = userID;
-
     }
+
+
     public String getFullName() {
         return fullName;
-
     }
     public void setFullName(String fullName) {
         this.fullName = fullName;
-
-
     }
+
+
     public String getEmail() {
         return email;
-
     }
     public void setEmail(String email) {
         this.email = email;
     }
+
+
     public LocalDate getBirthDate() {
         return birthDate;
     }
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
     }
+
+
     public double getBalance() {
         return balance;
     }
     public void setBalance(double balance) {
         this.balance = balance;
     }
+
+
     public LocalDate getCreatedAt() {
         return createdAt;
     }
     public void setCreatedAt(LocalDate createdAt) {
         this.createdAt = createdAt;
     }
+
+
     public LocalDate getUpdatedAt() {
         return updatedAt;
     }
@@ -71,6 +79,6 @@ public class User {
 
 
     public String toString() {
-        return "User{" + "userID=" + userID + ", fullName=" + fullName + ", email=" + email + ", birthDate=" + birthDate + ""
+        return "User{" + "userID=" + userID + ", fullName=" + fullName + ", email=" + email + ", birthDate=" + birthDate + ", createdAt=" + createdAt + ", updatedAt=" + updatedAt + '}';
     }
 }
